26df4f1c29998c0ce3b72d119b3bfc2cf328508c
sesana.federico <sesana.federico@TAVE-PC05.OSDBSESTO.local>
2020-11-10 13:02:58 +0100

    add endless loooop

---
+++ src/provagithu/ProvaGitHu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagithu;
+
+/**
+ *
+ * @author sesana.federico
+ */
+public class ProvaGitHu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        while(true){
+            System.out.println("Henlo " + i);
+            i++;
+        }
+    }
+    
+}
