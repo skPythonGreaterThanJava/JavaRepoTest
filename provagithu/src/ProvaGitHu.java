46236b612652b3122ee034d3a2cbbcf49473a7ea
sesana.federico <sesana.federico@TAVE-PC05.OSDBSESTO.local>
2020-11-17 13:16:30 +0100

    bello

---
+++ provagithu/src/ProvaGitHu.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *  ______________________________________________________________________
+ * |    _____________         _____________            ______________     |
+ * |   |                     |             |          |              |    |
+ * |   |                     |             |          |              |    |
+ * |   |                     |             |          |              |    |
+ * |   |                     |_____________|          |______________|    |
+ * |   |                     |      \                 |              |    |
+ * |   |                     |       \                |              |    |
+ * |   |                     |        \               |              |    |
+ * |   |                     |         \              |              |    |
+ * |   |                     |          \             |              |    |
+ * |   |                     |           \            |              |    |
+ * |   |_____________        |            \           |              |    |
+ * |______________________________________________________________________|
+ */
+
+/**
+ *
+ * @author sesana.federico
+ */
+public class ProvaGitHu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i = 69;
+        Bello bello = new Bello();
+        while(true){
+            System.out.println("henlo " + i);
+            i++;
+        }
+    }
+    
+}
